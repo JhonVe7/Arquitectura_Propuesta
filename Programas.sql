768462eb9a4c4dd804ac81cb4804d969c3148018
JhonVe7 <jvelascoriv@uniminuto.edu.co>
2020-05-20 22:48:55 -0500

    perro

---
+++ Programas.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Programas (
+id INTEGER(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+nombre VARCHAR(50) NOT NULL
+);
+
+INSERT INTO Programas(nombre) VALUES("Box"),("Crossfit"),("Aerobicos"),("Terapias"),("Acondicionamiento fisico"),("Cardio"),("Zumba");
